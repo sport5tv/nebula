f6bbefebc66c473701a8874277e2a39d6dbdf7f9
Martastain <martas@imm.cz>
2017-09-03 20:23:45 +0200

    temp name for saving

--- nx/cgclient.py
+++ nx/cgclient.py
@@ -8,23 +8,29 @@ def cg_download(target_path, method, **kwargs):
     start_time = time.time()
     target_dir = os.path.dirname(os.path.abspath(target_path))
     cg_server = config.get("cg_server", "https://cg.immstudios.org")
+
     if not os.path.isdir(target_dir):
         try:
             os.makedirs(target_dir)
         except Exception:
             logging.error("Unable to create output directory {}".format(target_dir))
             return False
+
     url = "{}/render/{}/{}".format(
             cg_server,
             config["site_name"],
             method
         )
+
     response = requests.get(url, params=kwargs)
     if response.status_code != 200:
         logging.error("CG Download failed with code {}".format(response.status_code))
         return False
-    with open(target_path, "wb") as f:
+
+    temp_path = target_path + ".creating"
+    with open(temp_path, "wb") as f:
         f.write(response.content)
+    os.rename(temp_path, target_path)
 
     logging.info("CG {} downloaded in {:.02f}s".format(method, time.time() - start_time))
     return True
